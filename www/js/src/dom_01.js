afb902baf3f45251a5e8733d9f434e17dde724d1
xido <erivery2@gmail.com>
2017-02-12 00:19:24 +0900

    album_01

--- www/js/src/dom_01.js
+++ www/js/src/dom_01.js
@@ -1,2 +1,12 @@
 /*! dom_01.js */
+// 사진첩 만들기 1
+
+ function showPic(whichpic){
+
+ 	var source = whichpic.getAttribute('href');
+ 	var placeholder = document.getElementById('placeholder');
+ 	placeholder.setAttribute('src', source);
+ 	return false;
+
+ };
 
